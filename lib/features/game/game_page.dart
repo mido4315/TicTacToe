aebff6b20b4e7797b5d896bdfc675da91065f63a
mido4315 <mohmed.mahmoud3@gmail.com>
2023-05-14 16:35:31 +0300

    add game page

--- lib/features/game/game_page.dart
+++ lib/features/game/game_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class GamePage extends StatelessWidget {
+  const GamePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
