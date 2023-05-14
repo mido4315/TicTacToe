15e48d503d78b1f40c9e7887e25c2d8073a67c73
mido4315 <mohmed.mahmoud3@gmail.com>
2023-05-14 21:07:27 +0300

    add fonts

---
+++ lib/core/constants/fonts.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MainFonts {
+  static var customFontWhite = GoogleFonts.coiny(
+    textStyle: const TextStyle(
+      color: Colors.white,
+      letterSpacing: 3,
+      fontSize: 28,
+    ),
+  );
+}
