11eaba206a179b44d24a5a72df2928ab1288c715
mido4315 <mohmed.mahmoud3@gmail.com>
2023-05-16 05:52:34 +0300

    add gradient background color

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -5,16 +5,23 @@ class MainColor {
   static Color secondaryColor = const Color(0xFFffca27);
   static Color accentColor = primaryColor;
   static Color winnerColor = const Color(0xFF4169e8);
+  static const Gradient backgroundGradient = LinearGradient(
+    begin: Alignment.topCenter,
+    end: Alignment.center,
+    colors: [
+      Color(0xFF5D5FEF),
+      Color(0xFF625BED),
+      Color(0xFF843CE0),
+    ],
+  );
 
-  // static Color primaryColor = const Color(0xFF606c38);
-  // static Color secondaryColor = const Color(0xFF283618);
-  // static Color accentColor = const Color(0xFFe9c46a);
-  // static Color winnerColor = const Color(0xFFfefae0);
+// static Color primaryColor = const Color(0xFF606c38);
+// static Color secondaryColor = const Color(0xFF283618);
+// static Color accentColor = const Color(0xFFe9c46a);
+// static Color winnerColor = const Color(0xFFfefae0);
 
-  // static Color primaryColor = const Color(0xFF393646);
-  // static Color secondaryColor = const Color(0xFF4F4557);
-  // static Color accentColor = const Color(0xFF393646);
-  // static Color winnerColor = const Color(0xFF6D5D6E);
-
-
+// static Color primaryColor = const Color(0xFF393646);
+// static Color secondaryColor = const Color(0xFF4F4557);
+// static Color accentColor = const Color(0xFF393646);
+// static Color winnerColor = const Color(0xFF6D5D6E);
 }
