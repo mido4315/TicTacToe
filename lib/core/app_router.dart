695297b3a73e28a16ce1c088448a7d708c8d3ba3
mido4315 <mohmed.mahmoud3@gmail.com>
2023-05-16 05:53:59 +0300

    create app_router.dart

---
+++ lib/core/app_router.dart
@@ -0,0 +1,32 @@
+import 'package:go_router/go_router.dart';
+import '../features/game/presentation/game_page.dart';
+import '../features/home/presentation/home_page.dart';
+import '../features/result/presentation/result_page.dart';
+import '../features/select game/presentation/select_game.dart';
+
+abstract class AppRouter {
+  static const kSelectGame = '/SelectGame';
+  static const kGamePage = '/GamePage';
+  static const kResultPage = '/ResultPage';
+
+  static final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const HomePage(),
+      ),
+      GoRoute(
+        path: kSelectGame,
+        builder: (context, state) => const SelectGame(),
+      ),
+      GoRoute(
+        path: kGamePage,
+        builder: (context, state) => const GamePage(),
+      ),
+      GoRoute(
+        path: kGamePage,
+        builder: (context, state) => const ResultPage(),
+      ),
+    ],
+  );
+}
